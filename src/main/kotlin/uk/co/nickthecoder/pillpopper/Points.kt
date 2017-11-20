77067152e6897554c726cd5ccd9483a17678f92d
Nick Robinson <nickthecoder@gmail.com>
2017-11-20 11:20:15 +0000

    Using Tickle's new "include" feature to allow mazes to be shared from one level to another.

--- src/main/kotlin/uk/co/nickthecoder/pillpopper/Points.kt
+++ src/main/kotlin/uk/co/nickthecoder/pillpopper/Points.kt
@@ -12,17 +12,17 @@ import uk.co.nickthecoder.tickle.util.Angle
 
 class Points : ActionRole() {
 
-    override fun createAction(): Action? {
+    override fun createAction(): Action {
         actor.y += 30.0
         actor.scale = 0.3
         val time = 0.5
         val stillTime = 0.2
         val growTime = 0.3
         val fadeDelay = 0.3
-        val rise = Forwards(actor.position, 60.0, Angle.degrees(90.0), time-stillTime, Eases.easeOut)
+        val rise = Forwards(actor.position, 60.0, Angle.degrees(90.0), time - stillTime, Eases.easeOut)
 
         val grow = Grow(actor, growTime, 1.0, Eases.easeOut)
-                .then(Grow(actor, time - growTime, 0.3 ,  Eases.easeIn))
+                .then(Grow(actor, time - growTime, 0.3, Eases.easeIn))
 
         val fade = Delay(fadeDelay).then(Fade(actor.color, time - fadeDelay, 1f, Eases.easeIn))
 
